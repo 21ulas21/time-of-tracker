48de86f81d6ef02f9e635a2ad3eea01a9374367b
21ulas21 <ulasegrk21@hotmail.com>
2023-08-08 20:16:35 +0300

    first
    first

---
+++ src/main/java/com/pinsoft/timeoftracker/TimeOfTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pinsoft.timeoftracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeOfTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeOfTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinsoft/timeoftracker/TimeOfTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinsoft.timeoftracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeOfTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
