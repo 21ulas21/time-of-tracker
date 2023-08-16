8fec9b9fda3928716461c40541ae491242d9509b
21ulas21 <ulasegrk21@hotmail.com>
2023-08-16 14:04:49 +0300

    Docker compose ve docker file gelistirmesi

---
+++ src/main/java/com/pinsoft/timeoftracker/library/exception/CustomExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.pinsoft.timeoftracker.library.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
+import java.util.Locale;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final MessageSource messageSource;
+
+/*
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> entityNotFoundExceptionRequest(EntityNotFoundException exception){
+
+
+        return new ResponseEntity<>("Kayıt Bulunamadı", HttpStatus.NOT_FOUND);
+    }
+    */
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
+    String errorMessage = "Entity not found";
+    return handleExceptionInternal(ex, errorMessage, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+}
+
+
+
+}
